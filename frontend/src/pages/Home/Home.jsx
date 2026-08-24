6174096388d003f84e1c396892b2e2a3da744771
Treasure123-school <treasurehomeschool@gmail.com>
2026-08-24 21:16:59 +0100

    Add PromoBanner styles and render it on Home

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Header from "@/components/Header/Header";
+import PromoBanner from "@/components/PromoBanner/PromoBanner";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <PromoBanner />
     </div>
   )
 }
